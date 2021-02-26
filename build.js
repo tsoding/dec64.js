7afc1dff61f39129a880bf93b59cf4476836ed43
rexim <reximkut@gmail.com>
2021-02-26 20:44:05 +0700

    Implement DEC64 packing and unpacking

--- build.js
+++ build.js
@@ -9,14 +9,54 @@ exec('wat2wasm dec64.wat', (error, stdout, stderr) => {
     console.error(stderr);
 });
 
-let result = '';
-for (let x of fs.readFileSync('dec64.wasm')) {
-    result += `${x},`;
-}
-fs.writeFileSync("dec64.js", `
-const bytes = new Uint8Array([${result}]);
-const mod = new WebAssembly.Module(bytes);
-const wasm = new WebAssembly.Instance(mod, { imports: {}});
+const MEM_INITIAL = 10;
+const MEM_MAXIMUM = 100;
 
-exports.add = wasm.exports.add;
+const mem = new WebAssembly.Memory({initial:MEM_INITIAL, maximum:MEM_MAXIMUM});
+const bytes = fs.readFileSync('dec64.wasm');
+const mod = new WebAssembly.Module(bytes);
+const wasm = new WebAssembly.Instance(mod, {
+    imports: {
+        mem: mem
+    }
+});
+
+let wasmByteArray = '';
+for (let x of bytes) {
+    wasmByteArray += `${x},`;
+}
+
+const noExports = ['toString', 'fromString'];
+
+let jsExports = '';
+for (let key of Object.keys(wasm.exports)) {
+    if (!noExports.includes(key)) {
+        jsExports += `exports.${key} = wasm.exports.${key};\n`;
+    }
+}
+
+fs.writeFileSync("dec64.js", `
+const mem = new WebAssembly.Memory({initial:${MEM_INITIAL}, maximum:${MEM_MAXIMUM}});
+const bytes = new Uint8Array([${wasmByteArray}]);
+const mod = new WebAssembly.Module(bytes);
+const wasm = new WebAssembly.Instance(mod, {
+    imports: {
+        mem: mem
+    }
+});
+
+${jsExports}
+
+exports.toString = () => {
+    console.error('toString: not implemented');
+};
+
+exports.fromString = () => {
+    console.error('fromString: not implemented');
+};
+
+exports.unpack = (x) => [
+    wasm.exports.unpackCoeff(x),
+    wasm.exports.unpackExp(x)
+];
 `);
--- dec64.wat
+++ dec64.wat
@@ -1,13 +1,30 @@
 (module
- ;; (memory (import "js" "mem") 1)
- (func $getExp (export "getExp")
+ (memory (import "imports" "mem") 1)
+ (func $pack (export "pack")
+       (param $coeff i64)
+       (param $exp i64)
+       (result i64)
+       (i64.or
+        (i64.shl
+         (get_local $coeff)
+         (i64.const 8))
+        (i64.and
+         (get_local $exp)
+         (i64.const 0xFF))))
+ (func $unpackExp (export "unpackExp")
        (param $a i64)
        (result i64)
-       (i64.const 69))
- (func $getCoeff (export "getCoeff")
+       (i64.shr_s
+        (i64.shl
+         (get_local $a)
+         (i64.const 56))
+        (i64.const 56)))
+ (func $unpackCoeff (export "unpackCoeff")
        (param $a i64)
        (result i64)
-       (i64.const 69))
+       (i64.shr_s
+        (get_local $a)
+        (i64.const 8)))
  (func $toString (export "toString")
        (param $ptr i64)
        (result i64)
